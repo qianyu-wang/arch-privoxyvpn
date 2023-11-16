6ce51a89bf7ce83db6724ce40e1ed472bbd34045
Yogurt <22412010+qianyu-wang@users.noreply.github.com>
2023-11-16 01:31:24 +0000

    log singbox command
    Signed-off-by: Yogurt <22412010+qianyu-wang@users.noreply.github.com>

--- run/nobody/singbox.sh
+++ run/nobody/singbox.sh
@@ -4,6 +4,8 @@ echo "[info] Attempting to start singbox..."
 
 singbox_cli="nohup /home/nobody/sing-box run -c ${SINGBOX_CONFIG}"
 
+echo ${singbox_cli}
+
 ${singbox_cli} &
 
 echo "[info] singbox process started"
